231ca26962d3fc4d5e5971123ae2599f84101b48
JeppeLarsen <Pnb97hhv!>
2020-02-25 11:44:37 +0100

    Simpel brugerautorisation implementeret

--- src/galgeleg/Galgeserver.java
+++ src/galgeleg/Galgeserver.java
@@ -6,13 +6,37 @@ import java.rmi.Naming;
 public class Galgeserver {
 
     public static void main(String[] arg) throws Exception {
+
         // Enten: Kør programmet 'rmiregistry' fra mappen med .class-filerne, eller:
         java.rmi.registry.LocateRegistry.createRegistry(1099); // start i server-JVM
 
-        Galgelogik galgelogik = new Galgelogik();
-        Naming.rebind("rmi://localhost:1099/galgeleg", galgelogik);
-        System.out.println("Kontotjeneste registreret.");
 
+        //modtager bruger og adgangskode fra klienten
+
+
+        //Tjek om bruger og adgangskode er korrekt
+        //Hvis brugeren er autoriseret
+
+        if (autoriserBruger("Jeppe", "Mads")) {
+
+            Galgelogik galgelogik = new Galgelogik();
+            Naming.rebind("rmi://localhost:1099/galgeleg", galgelogik);
+            System.out.println("Galgeleg registreret.");
+
+        }
     }
 
+    public static boolean autoriserBruger(String brugernavn, String adgangskode) {
+
+        if (brugernavn.equals("Jeppe") && adgangskode.equals("Mads")) {
+            return true;
+
+        } else {
+            return false;
+        }
+
+
+    }
 }
+
+
